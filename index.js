842fcc0f811a2c9faa5e79a35ed1dbffca5d18d4
Nico Clack <16543008+hedgehog125@users.noreply.github.com>
2024-11-10 22:24:11 +0000

    Environment variable checks

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ import {
 } from "./src/subFns.js";
 import { logWhenResolved } from "./src/lib.js";
 
+[
+	"SITE_URL",
+	"MOUNT_PATH",
+	"GIF_URL",
+	"RENDER_X",
+	"RENDER_Y",
+	"CANVAS_WIDTH",
+	"CANVAS_HEIGHT",
+	"PLAYBACK_SPEED",
+].forEach((envVarName) => {
+	if (!process.env.hasOwnProperty(envVarName)) {
+		throw new Error(`Environment variable ${envVarName} has not been set.`);
+	}
+});
+
 const state = await loadState();
 if (state.errorOccurredAt != null) {
 	throw new Error("Exiting as an error previously occurred...");
