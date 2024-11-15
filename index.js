79c72b00854062996d8977be2f2f22b2a715a825
Nico Clack <16543008+hedgehog125@users.noreply.github.com>
2024-11-15 09:33:08 +0000

    Uncomment

--- index.js
+++ index.js
@@ -1,104 +1,103 @@
-console.log("Test");
-// // TODO: use gifuct-js?
+// TODO: use gifuct-js?
 
-// import "dotenv/config";
-// import {
-// 	loadPage,
-// 	loadState,
-// 	panic,
-// 	prepareGif,
-// 	renderFrame,
-// 	saveState,
-// 	shutdownBrowserWithTimeout,
-// } from "./src/subFns.js";
-// import { timeoutRace } from "./src/lib.js";
+import "dotenv/config";
+import {
+	loadPage,
+	loadState,
+	panic,
+	prepareGif,
+	renderFrame,
+	saveState,
+	shutdownBrowserWithTimeout,
+} from "./src/subFns.js";
+import { timeoutRace } from "./src/lib.js";
 
-// [
-// 	"SITE_URL",
-// 	"MOUNT_PATH",
-// 	"GIF_URL",
-// 	"RENDER_X",
-// 	"RENDER_Y",
-// 	"CANVAS_WIDTH",
-// 	"CANVAS_HEIGHT",
-// 	"PLAYBACK_SPEED",
-// 	"USE_NIXPACKS_PUPPETEER_ARGS",
-// 	"MAX_INITIAL_WAIT",
-// 	"MAX_ERRORS",
-// 	"MAX_RETRIES",
-// ].forEach((envVarName) => {
-// 	if (!process.env.hasOwnProperty(envVarName)) {
-// 		throw new Error(`Environment variable ${envVarName} has not been set.`);
-// 	}
-// });
+[
+	"SITE_URL",
+	"MOUNT_PATH",
+	"GIF_URL",
+	"RENDER_X",
+	"RENDER_Y",
+	"CANVAS_WIDTH",
+	"CANVAS_HEIGHT",
+	"PLAYBACK_SPEED",
+	"USE_NIXPACKS_PUPPETEER_ARGS",
+	"MAX_INITIAL_WAIT",
+	"MAX_ERRORS",
+	"MAX_RETRIES",
+].forEach((envVarName) => {
+	if (!process.env.hasOwnProperty(envVarName)) {
+		throw new Error(`Environment variable ${envVarName} has not been set.`);
+	}
+});
 
-// const state = await timeoutRace(loadState(), 10 * 1000);
-// if (state.completed) {
-// 	throw new Error("Already completed playback");
-// }
-// if (state.errorCount > Number(process.env.MAX_ERRORS)) {
-// 	throw new Error(
-// 		`Exiting as MAX_ERRORS (${process.env.MAX_ERRORS}) has been reached. Last panic was at ${state.errorOccurredAt}`
-// 	);
-// }
+const state = await timeoutRace(loadState(), 10 * 1000);
+if (state.completed) {
+	throw new Error("Already completed playback");
+}
+if (state.errorCount > Number(process.env.MAX_ERRORS)) {
+	throw new Error(
+		`Exiting as MAX_ERRORS (${process.env.MAX_ERRORS}) has been reached. Last panic was at ${state.errorOccurredAt}`
+	);
+}
 
-// let browser, page, pixels, paletteButtons, errorEventPromise;
+let browser, page, pixels, paletteButtons, errorEventPromise;
 
-// function onUncaughtException(err) {
-// 	process.removeListener("uncaughtException", onUncaughtException);
-// 	panic(browser, state, err);
-// }
-// process.on("uncaughtException", onUncaughtException);
+function onUncaughtException(err) {
+	process.removeListener("uncaughtException", onUncaughtException);
+	panic(browser, state, err);
+}
+process.on("uncaughtException", onUncaughtException);
 
-// console.log("Preparing GIF...");
-// const preparedGif = await prepareGif(state);
-// if (preparedGif == null) {
-// 	await saveState(state);
-// 	process.exit();
-// }
+console.log("Preparing GIF...");
+const preparedGif = await prepareGif(state);
+if (preparedGif == null) {
+	await saveState(state);
+	process.exit();
+}
 
-// const { pixelIds, width } = preparedGif;
+const { pixelIds, width } = preparedGif;
 
-// const timeoutTask = setTimeout(async () => {
-// 	throw new Error("Timeout exceeded");
-// }, 150 * 1000);
+const timeoutTask = setTimeout(async () => {
+	throw new Error("Timeout exceeded");
+}, 150 * 1000);
 
-// let retries = 0;
-// while (true) {
-// 	let failed = false;
-// 	try {
-// 		console.log("Loading page...");
-// 		({ browser, page, pixels, paletteButtons, errorEventPromise } =
-// 			await loadPage());
+let retries = 0;
+while (true) {
+	let failed = false;
+	try {
+		console.log("Loading page...");
+		({ browser, page, pixels, paletteButtons, errorEventPromise } =
+			await loadPage());
 
-// 		console.log("Rendering...");
-// 		const changedPixels = await Promise.race([
-// 			renderFrame(pixelIds, width, pixels, paletteButtons, page),
-// 			errorEventPromise,
-// 		]);
-// 		console.log(`Changed ${changedPixels} pixels`);
-// 	} catch (error) {
-// 		try {
-// 			await shutdownBrowserWithTimeout(browser);
-// 		} catch {}
+		console.log("Rendering...");
+		const changedPixels = await Promise.race([
+			renderFrame(pixelIds, width, pixels, paletteButtons, page),
+			errorEventPromise,
+		]);
+		console.log(`Changed ${changedPixels} pixels`);
+	} catch (error) {
+		try {
+			await shutdownBrowserWithTimeout(browser);
+		} catch {}
 
-// 		console.log("An error occurred while loading and rendering the page:");
-// 		console.error(error);
-// 		failed = true;
-// 	}
+		console.log("An error occurred while loading and rendering the page:");
+		console.error(error);
+		failed = true;
+	}
 
-// 	if (!failed) break;
-// 	retries++;
-// 	if (retries > Number(process.env.MAX_RETRIES)) {
-// 		throw new Error("Max retries exceeded");
-// 	}
-// 	console.log(`Starting retry ${retries} of ${process.env.MAX_RETRIES}...`);
-// }
+	if (!failed) break;
+	retries++;
+	if (retries > Number(process.env.MAX_RETRIES)) {
+		throw new Error("Max retries exceeded");
+	}
+	console.log(`Starting retry ${retries} of ${process.env.MAX_RETRIES}...`);
+}
 
-// console.log("Shutting down...");
-// await shutdownBrowserWithTimeout(browser);
-// await saveState(state);
-// console.log("Done");
-// clearTimeout(timeoutTask);
+console.log("Shutting down...");
+await shutdownBrowserWithTimeout(browser);
+await saveState(state);
+console.log("Done");
+clearTimeout(timeoutTask);
 
-// // TODO: this isn't exiting properly!!!! Must not be clearing all the timeouts
+// TODO: this isn't exiting properly!!!! Must not be clearing all the timeouts
